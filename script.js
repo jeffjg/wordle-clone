d726e49435d31d544e4ef6e6e45a6e0282fd30bf
Jeff Garrett <jeffjgarrett@gmail.com>
2024-10-22 13:16:25 -0500

    feat: Sets up basic navigation of typing a letter to progress it and backspace to remove/go back.

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
+let userGuess = document.querySelector(".user-guess");
+let inputOptions = userGuess.getElementsByTagName("input");
 
+console.log("here are the input options: ", inputOptions);
+// Progresses the form forward when a user inputs a letter and backwards when user presses Backspace.
+
+Array.from(inputOptions).forEach(function (input) {
+  let letterGuess = input.classList.contains("letter-choice");
+  input.addEventListener("keydown", (event) => {
+    if (event.key.length === 1 && event.key.match(/[a-zA-Z]/) && letterGuess) {
+      input.value = event.key.toUpperCase();
+      event.preventDefault();
+      if (input.nextElementSibling) {
+        input.nextElementSibling.focus();
+      }
+    }
+
+    if (event.key === "Backspace") {
+      if (input.value !== "" && letterGuess) {
+        input.value = "";
+        event.preventDefault();
+      } else {
+        if (input.previousElementSibling) {
+          input.previousElementSibling.focus();
+        }
+      }
+    }
+  });
+});
